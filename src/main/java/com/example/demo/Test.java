c61f88ef446dcede8a7fb6cc5ed188f80cbb6108
1053113990@qq.com <1053113990@qq.com>
2019-02-26 23:17:53 +0800

    demo

---
+++ src/main/java/com/example/demo/Test.java
@@ -0,0 +1,33 @@
+package com.example.demo;
+
+
+import java.io.IOException;
+import java.lang.reflect.Field;
+import java.util.Scanner;
+
+public class Test {
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+//        try {
+//            long start = System.currentTimeMillis();
+//            Process process = Runtime.getRuntime().exec(
+//                    new String[] { "wmic", "cpu", "get", "ProcessorId" });
+//            process.getOutputStream().close();
+//            Scanner sc = new Scanner(process.getInputStream());
+//            String property = sc.next();
+//            String serial = sc.next();
+//            System.out.println(property + ": " + serial);
+//            System.out.println("time:" + (System.currentTimeMillis() - start));
+//        } catch (IOException e) {
+//            // TODO Auto-generated catch block
+//            e.printStackTrace();
+//        }
+//        Class<?> clazz = Test123.class;
+//        Field[] ff = clazz.getDeclaredFields();
+//        for(Field f : ff){
+//            char[] chars = f.getName().toCharArray();
+//            chars[0] -= 32;
+//            System.out.println("oqp.set"+String.copyValueOf(chars)+"()");
+//        }
+    }
+}
